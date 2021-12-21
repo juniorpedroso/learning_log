f77ca03c3decf716e2c536d851067a2a804c52ab
Junior Pedroso <sintasebemagora@gmail.com>
2021-12-21 13:49:25 -0300

    Update

---
+++ learning_logs/urls.py
@@ -0,0 +1,9 @@
+'''Define padrões de URL para learning_logs.'''
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    '''A página inicial de learning_log'''
+    return render(request, 'learning_logs/index.html')
+
